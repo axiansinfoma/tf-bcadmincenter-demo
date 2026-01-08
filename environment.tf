e821cad4d2ed215da2bd6595091b523189ec70c1
Michael Villani <45660891+michvllni@users.noreply.github.com>
2026-01-08 11:36:23 +0000

    feat: add test env

---
+++ environment.tf
@@ -0,0 +1,6 @@
+resource "bcadmincenter_environment" "dev" {
+  name               = "dev"
+  type               = "Sandbox"
+  country_code       = "US"
+  application_family = "BusinessCentral"
+}
