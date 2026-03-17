32e9bc1a5a80075f7fb8fe8ca0e43c0b466daf22
Michael Villani <45660891+michvllni@users.noreply.github.com>
2026-03-17 13:05:10 +0000

    fix: update Azure secrets references in Terraform workflow and add application version to environment resource

--- src/environment.tf
+++ src/environment.tf
@@ -3,8 +3,9 @@ data "bcadmincenter_application_family" "business_central" {
 }
 
 resource "bcadmincenter_environment" "example" {
-  name               = "demo_mivi"
-  country_code       = "DE"
-  type               = "Sandbox"
-  application_family = data.bcadmincenter_application_family.business_central.name
+  name                = "demo_mivi"
+  country_code        = "DE"
+  type                = "Sandbox"
+  application_version = "27.5"
+  application_family  = data.bcadmincenter_application_family.business_central.name
 }
