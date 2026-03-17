0f02e592c94a127ed8c5810fa8ce10d95606107a
Michael Villani <45660891+michvllni@users.noreply.github.com>
2026-03-17 16:36:55 +0100

    feat: add settings to demo environment (#9)

--- src/environment.tf
+++ src/environment.tf
@@ -7,4 +7,10 @@ resource "bcadmincenter_environment" "example" {
   country_code       = "DE"
   type               = "Sandbox"
   application_family = data.bcadmincenter_application_family.business_central.name
+
+  settings {
+    update_window_start_time = "22:00"
+    update_window_end_time   = "04:00"
+    update_window_timezone   = "W. Europe Standard Time"
+  }
 }
